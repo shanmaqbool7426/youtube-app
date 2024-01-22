7fbd83454eae5b6afbd27c8689bb8e3b322fb3f2
Shan Maqbool <shan.maqbool@ceative.co.uk>
2024-01-22 01:14:24 -0800

    fix multer issue

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,26 @@
-import multer from "multer";
+// import multer from "multer";
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "./public/temp")
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.originalname)
+//     }
+//   })
+  
+// export const upload = multer({ 
+//     storage, 
+// })
+
+// console.log("upload",upload,storage)
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname)
-    }
-  })
-  
-export const upload = multer({ 
-    storage, 
-})
+  destination: function(req, file, cb) {
+    cb(null, __dirname);
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
+});
 
-console.log("upload",upload,storage)
